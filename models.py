2b32b603170e028dacce533afcdf0e999314f3cf
kevin cheung <kevin2012uk@gmail.com>
2023-08-07 20:19:47 +0100

    Success!!!! - need to increase complexity of model

--- models.py
+++ models.py
@@ -19,21 +19,23 @@ class Conv3D_LSTM(torch.nn.Module):
 
         # CNN layers
         self.cnn = nn.Sequential(
-            nn.Conv3d(input_dim, 64, kernel_size=3, padding=1),
-            # nn.BatchNorm3d(64),
+            nn.Conv3d(input_dim, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
+            nn.BatchNorm3d(32),
             nn.MaxPool3d(kernel_size=2, stride=2),
-            nn.Conv3d(64, 64, kernel_size=3, padding=1),
-            nn.Conv3d(64, 64, kernel_size=3, padding=1),
-            nn.Conv3d(64, 128, kernel_size=3, padding=1),
-            nn.Conv3d(128, 128, kernel_size=3, padding=1),
-            nn.Conv3d(128, 256, kernel_size=3, padding=1),
-            nn.Conv3d(256, 256, kernel_size=3, padding=1)
+            nn.Conv3d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(32),
+            nn.MaxPool3d(kernel_size=2, stride=2),
+            nn.Conv3d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(32),
+            nn.MaxPool3d(kernel_size=2, stride=2),
         )
 
         self.dropout = nn.Dropout(dropout_rate)
 
-        self.lstm = nn.LSTM(32768, 100,num_layers=2, batch_first=True)
+        self.lstm = nn.LSTM(64, 100,num_layers=2, batch_first=True)
 
         self.fc = nn.Linear(100, num_classes)
 
