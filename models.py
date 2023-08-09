ff75f2f24a2df81f21f3197244495703fbfc6f68
kevin cheung <kevin2012uk@gmail.com>
2023-08-09 11:06:27 +0100

    edited some comments

--- models.py
+++ models.py
@@ -26,20 +26,16 @@ class Conv3D_LSTM(torch.nn.Module):
             nn.Conv3d(32, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.BatchNorm3d(32),
-            # nn.MaxPool3d(kernel_size=2, stride=2),
+            nn.MaxPool3d(kernel_size=2, stride=2),
             nn.Conv3d(32, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.BatchNorm3d(32),
-            # nn.MaxPool3d(kernel_size=2, stride=2),
-            nn.Conv3d(32, 32, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm3d(32),
-            # nn.MaxPool3d(kernel_size=2, stride=2),
+            nn.MaxPool3d(kernel_size=2, stride=2),
         )
 
         self.dropout = nn.Dropout(dropout_rate)
 
-        self.lstm = nn.LSTM(4096, 100,num_layers=2, batch_first=True)
+        self.lstm = nn.LSTM(64, 100,num_layers=2, batch_first=True)
 
         self.fc = nn.Linear(100, num_classes)
 
